49d84fe8d62f895c0aa93eb788d91c23c57b2456
kashiwa8989 <119640627+kashiwa8989@users.noreply.github.com>
2023-02-19 12:36:51 +0000

    レイアウト5

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,7 +7,7 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
-    @books = @user.books.where(is_draft: false).order(created_at: :desc).page(params[:page]).per(8)
+    @books = @user.books.where(is_draft: false).order(created_at: :desc).page(params[:page]).per(9)
     # @books = Book.where(is_draft: :true).order("created_at DESC").page(params[:page]).per(8)
   end
   # .page(params[:page]).per(8)
